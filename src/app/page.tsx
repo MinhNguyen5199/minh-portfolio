742994ac953000a4931abc4affb8bb72df4a27f2
MinhNguyen5199 <hoangminhabc11@gmail.com>
2024-02-29 21:42:36 -0700

    final version

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 
 import AboutMe from "../../components/AboutMe";
 import Contact from "../../components/Contact";
-import Experience from "../../components/Experience";
+// import Experience from "../../components/Experience";
 import Footer from "../../components/Footer";
 import Projects from "../../components/Projects";
 import Skills from "../../components/Skills";
